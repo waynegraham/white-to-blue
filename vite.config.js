68d17621b614745eaf82ff5180b150bd86b45e92
Wayne Graham <wgraham@clir.org>
2024-11-07 17:15:06 -0800

    Update base for vite build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/white-to-blue',
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+  }
 })
